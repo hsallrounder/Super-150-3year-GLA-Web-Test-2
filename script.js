cb097682f07d227aed7ecaea2aca73402d59adad
hsallrounder <hsallrounder@gmail.com>
2022-11-25 22:44:08 +0530

    theme changer

--- script.js
+++ script.js
@@ -1,22 +1,51 @@
 const xhr = new XMLHttpRequest;
 
+// API
+
 xhr.onload = function(){
     var data = JSON.parse(this.response);
-    const movie_grid = document.getElementById('display-karlo');
-    movie_grid.innerHTML = ``;
+    const movie = document.getElementById('display-karlo');
+    movie.innerHTML = ``;
     for(var i = 0; i < data.length; i++) {
-        var newItem = document.createElement('img')
-        newItem.src = data[i].show.image.medium;
-        movie_grid.appendChild(newItem);
+        var newImg = document.createElement('img')
+        newImg.src = data[i].show.image.medium;
+        movie.appendChild(newImg);
     }
     console.log(data);
 }
 
 
 document.getElementById('search-button').addEventListener('click',(e) => {
+    e.preventDefault();
     let input = document.getElementById('input').value;
     const url = " https://api.tvmaze.com/search/shows?q=" + input;
     xhr.open("GET",url);
-    e.preventDefault();
     xhr.send();
 })
+
+
+// Theme -> Local Storage
+
+const themeColors = document.querySelectorAll('[name="theme"]');
+
+const storeTheme = function (theme) {
+  localStorage.setItem("theme", theme);
+  console.log("theme applied");
+};
+
+const applyTheme = function () {
+  const activeTheme = localStorage.getItem("theme");
+
+  themeColors.forEach((themeOption) => {
+    if (activeTheme == themeOption.id) {
+      themeOption.checked = true;
+    }
+  });
+};
+themeColors.forEach((themeOption) => {
+  themeOption.addEventListener("click", () => {
+    storeTheme(themeOption.id);
+  });
+});
+
+document.onload = applyTheme();
